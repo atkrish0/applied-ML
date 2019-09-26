dd53c6dfc8100dd703239387e962646c71befb21
Atheesh Krishnan <atheesh.98@gmail.com>
2019-09-27 00:04:01 +0530

    Add files via upload

--- diabetes_onset.py
+++ diabetes_onset.py
@@ -6,4 +6,25 @@ import keras
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
 names = ['n_pregnant', 'glucose_concentration', 'blood_pressuer (mm Hg)', 'skin_thickness (mm)', 'serum_insulin (mu U/ml)',
         'BMI', 'pedigree_function', 'age', 'class']
+
 df = pd.read_csv(url, names = names)
+df[df['glucose_concentration'] == 0]
+
+columns = ['glucose_concentration',
+           'blood_pressuer (mm Hg)', 'skin_thickness (mm)', 'serum_insulin (mu U/ml)', 'BMI']
+
+for col in columns:
+    df[col].replace(0, np.NaN, inplace=True)
+
+# Drop rows with missing values
+df.dropna(inplace=True)
+dataset = df.values
+print(dataset.shape)
+
+X = dataset[:,0:8]
+y = dataset[:,8].astype(int)
+
+# Normalize the data using sklearn StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler().fit(X)
