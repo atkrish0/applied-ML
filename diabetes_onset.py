7e29cc8a748fa0d6763b38739863a23406d84fbf
Atheesh Krishnan <atheesh.98@gmail.com>
2019-09-27 00:14:40 +0530

    Add files via upload

--- diabetes_onset.py
+++ diabetes_onset.py
@@ -45,6 +45,31 @@ def create_model:
     # compile
     adam = Adam(lr = 0.01)
     model.compile(loss = 'binary_crossentropy', optimizer = adam, metrics = ['accuracy'])
+    return model
 
-model = create_model()
-print(model.summary())
+model_1 = create_model()
+print(model_1.summary())
+
+# Do a grid search for the optimal batch size and number of epochs
+model_2 = KerasClassfier(build_fn = create_model, verbose = 1)
+
+# define the grid search parameters
+batch_size = [10, 20, 40]
+epochs = [10, 50, 100]
+
+# make a dictionary of the grid search parameters
+param_grid = dict(batch_size=batch_size, epochs=epochs)
+
+# build and fit the GridSearchCV
+grid = GridSearchCV(estimator=model, param_grid=param_grid,
+                    cv=KFold(random_state=seed), verbose=10)
+grid_results = grid.fit(X_standardized, Y)
+
+# summarize the results
+print("Best: {0}, using {1}".format(
+    grid_results.best_score_, grid_results.best_params_))
+means = grid_results.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_results.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print('{0} ({1}) with: {2}'.format(mean, stdev, param))
