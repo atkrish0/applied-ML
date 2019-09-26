8c5113f0909ab8863438bd8c886bc0595cc5abd2
Atheesh Krishnan <atheesh.98@gmail.com>
2019-09-27 00:11:18 +0530

    Add files via upload

--- diabetes_onset.py
+++ diabetes_onset.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
-import keras
+from sklearn.model_selection import KFold, GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrapers.scikit_learn import KerasClassifier
+from keras.optimizers import Adam
 
 # import the uci pima indians diabetes dataset
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
@@ -26,5 +30,21 @@ y = dataset[:,8].astype(int)
 
 # Normalize the data using sklearn StandardScaler
 from sklearn.preprocessing import StandardScaler
-
 scaler = StandardScaler().fit(X)
+
+# Transform and display the training data
+X_standardized = scaler.transform(X)
+data = pd.DataFrame(X_standardized)
+
+def create_model:
+    # create
+    model = Seqential()
+    model.add(Dense(8, input_dim=8, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(4, input_dim=8, kernel_initializer='normal',activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile
+    adam = Adam(lr = 0.01)
+    model.compile(loss = 'binary_crossentropy', optimizer = adam, metrics = ['accuracy'])
+
+model = create_model()
+print(model.summary())
